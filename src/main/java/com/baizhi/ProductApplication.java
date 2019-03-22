d3ea27b7be38d96cfaf31a84fec4f5880016165a
zly <zly1992zly@126.com>
2019-03-22 10:16:45 +0800

    商品注册中心v1.0

---
+++ src/main/java/com/baizhi/ProductApplication.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
